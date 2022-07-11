96f5377c2f3e30b42ae70bb9cdec193592a07c55
MulqiGaming64 <mulqi311@gmail.com>
2022-07-11 13:49:05 +0700

    Change Config file Update to PM4

--- README.md
+++ README.md
@@ -3,9 +3,32 @@
 # CustomSetting
 This Plugin gives you a setting page for your Server :)
 
+# Screenshot
+
+![Screenshot](https://raw.githubusercontent.com/MulqiGaming64/CustomSetting/master/Screenshot.jpg)
+
+# Config
+
+``` YAML
+
+# CustomSettings Config
+# Change Title of your Settings here
+title: "MyServer Settings"
+
+# Change Content of your Settings here
+# Use {name} for Name Player
+# Use {line} For new Line
+contents: "Welcome to Our server, MulqiGaming64{line}{line}Hope you can be comfortable :)."
+
+# Change icon of Your Settings here
+# Can url or path
+icon: "textures/ui/settings_glyph_color_2x"
+```
+
 ## Thanks To
  * [Nikipuh](https://github.com/nikipuh) For Making this Plugin
 
 ## Note
  * I just updated this plugin to Pocketmine 4.0.0
+ * I've tried to add toggles, inputs and others but it gets canceled when entering data.
  * If there is an issue or error, please push the Issue [Here](https://github.com/MulqiGaming64/CustomSetting/pulls)
---
+++ src/CustomSetting/Main.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CustomSetting;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+
+use pocketmine\player\Player;
+use pocketmine\network\mcpe\protocol\ServerSettingsResponsePacket;
+
+class Main extends PluginBase implements Listener {
+
+	public function onEnable(): void{
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+	
+	/** Getting content and Icon from Config */
+	public function getSetting(Player $player): string{
+		// Title
+		$title = $this->getConfig()->get("title");
+		// Icon
+		$icon = $this->getConfig()->get("icon");
+		$iconType = filter_var($icon, FILTER_VALIDATE_URL) ? "url" : "path";
+		// Contents
+		$content = str_replace(
+			["{name}", "{line}"],
+			[$player->getName(), "\n"],
+			$this->getConfig()->get("contents")
+		);
+		
+		// ServerSettings
+		$formData = [
+    		"type" => "custom_form",
+    		"title" => $title,
+    		"icon" => [
+    			"type" => $iconType,
+    			"data" => $icon
+    		],
+    		"content" => [
+    			[
+    				"type" => "label",
+    				"text" => $content
+    			]
+    		]
+    	];
+    
+    	return json_encode($formData);
+	}
+	
+	/** @var DataPacketReceiveEvent $event */
+	public function onReceive(DataPacketReceiveEvent $event): void{
+		// Why no ServerSettingsRequestPacket?, I've tried but the settings don't show up
+    	if(!$event->getOrigin()->getPlayer() instanceof Player) return;
+    
+    	$net = $event->getOrigin();
+    	$player = $net->getPlayer();
+    	$packet = ServerSettingsResponsePacket::create(5928, $this->getSetting($player));
+    	$net->sendDataPacket($packet);
+    }
+}
